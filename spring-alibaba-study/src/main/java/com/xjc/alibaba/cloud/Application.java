1bed44f57f18f4f15cf61d48abcc4abf0707c920
xiongjc <2323948060@qq.com>
2020-06-13 17:18:19 +0800

    alibaba-cloud

---
+++ spring-alibaba-study/src/main/java/com/xjc/alibaba/cloud/Application.java
@@ -0,0 +1,17 @@
+package com.xjc.alibaba.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author XiongJinChen
+ * @version 1.0
+ * @date 2020/6/13 17:02
+ * @name Application
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
